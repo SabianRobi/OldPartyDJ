4e98ffd1d0edbe5849aeb95bf74867b66c10c26a
SabianRobi <sabian.robi@gmail.com>
2022-12-17 21:49:41 +0100

    Added dark mode selector

--- resources/views/layouts/common/footer.blade.php
+++ resources/views/layouts/common/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+<footer class="p-4 bg-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         ©
         <?= date("Y") ?>
