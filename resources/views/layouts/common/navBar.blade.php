55e3a155c560a8ad692bb98a86a4a1e965f9979f
SabianRobi <sabian.robi@gmail.com>
2023-03-11 22:21:14 +0100

    Made current page highlites in navbar

--- resources/views/layouts/common/navBar.blade.php
+++ resources/views/layouts/common/navBar.blade.php
@@ -110,8 +110,8 @@
                 class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-blue-200 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
                     <a href="{{ route('home') }}"
-                        class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                        aria-current="page">Home</a>
+                        class="block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ request()->routeIs('home') ? 'bold text-black dark:text-white' : 'text-gray-400' }}">
+                        Home</a>
                 </li>
                 {{-- <li>
                     <a href="#"
@@ -120,7 +120,7 @@
                 </li> --}}
                 <li>
                     <a href="{{ route('landingParty') }}"
-                        class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Party
+                        class="block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 {{ request()->is('party*') ? 'bold text-black dark:text-white' : 'text-gray-400' }}">Party
                         Time</a>
                 </li>
             </ul>
