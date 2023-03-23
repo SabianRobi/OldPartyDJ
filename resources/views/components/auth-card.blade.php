00e29fc0942b142b0ca3194cc47e7161bb4483d8
SabianRobi <sabian.robi@gmail.com>
2023-03-23 20:38:15 +0100

    Added go back button for forms

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,6 +1,6 @@
 @props(['title'])
 
-<div class="flex flex-col sm:justify-center items-center py-2">
+<div class="flex flex-col sm:justify-center py-2">
     {{-- <div>
         {{ $logo }}
     </div> --}}
@@ -9,5 +9,11 @@
 
     <div class="w-full sm:max-w-md px-6 py-4 bg-white dark:bg-gray-700 shadow-md overflow-hidden sm:rounded-lg">
         {{ $slot }}
+    </div>
+    <div class="mt-1 text-left dark:text-gray-300">
+        <a href="{{ url()->previous() }}" class="">
+                <img src="{{ asset('/images/misc/arrow-left-solid.svg') }}" alt="Go back" class="h-5 inline mr-1">
+                Go back
+        </a>
     </div>
 </div>
